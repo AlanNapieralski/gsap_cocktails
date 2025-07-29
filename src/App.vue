967b1c806ff91089c4f3ce3850c8abb09234f7a8
AlanNapieralski <a.napieralski@lancaster.ac.uk>
2025-07-29 16:04:29 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import { gsap } from 'gsap'
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+</script>
+
+<template>
+  <div class="flex-center h-screen">
+    <h1 class="text-3xl text-indigo-300">Hello, GSAP!</h1>
+  </div>
+</template>
