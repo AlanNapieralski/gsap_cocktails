91a1c67960495227a8987f4d6093e98f8e43441f
AlanNapieralski <a.napieralski@lancaster.ac.uk>
2025-07-29 22:22:31 +0100

    updated base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jsm_gsap_landing/',
+  base: '/',
   plugins: [
     vue(),
     tailwindcss(),
